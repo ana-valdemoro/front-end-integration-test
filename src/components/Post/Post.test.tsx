de43b9b20efe78f6a6dd145a8eb3a8a910e00615
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-07-11 18:37:54 +0100

    FTR: added unit test to post component

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-
 import { act, render, screen, waitFor } from "@testing-library/react";
-
+import userEvent from "@testing-library/user-event";
 import { getPostById } from '../../services/postService';
 
-import { SearchPost } from "./SearchPost";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useParams } from "react-router-dom";
 
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 
 import { Post } from "./Post";
 
@@ -17,6 +14,7 @@ import { Post } from "./Post";
 afterEach(() => {
 
     (getPostById as jest.Mock).mockReset();
+    // (useParams as jest.Mock).mockReset();
 
 });
 
@@ -30,17 +28,15 @@ jest.mock("../../services/postService", () => ({
 
 
 
-const mockUseParams = jest.fn();
+// const mockUseParams = jest.fn();
 
 jest.mock('react-router-dom', () => ({
 
     ...jest.requireActual('react-router-dom') as any,
 
-    useParams: () => mockUseParams,
+    useParams: jest.fn(),
 
 }));
-
-
 
 const mockPost = {
 
@@ -50,42 +46,37 @@ const mockPost = {
 
     body: "bodychupipandi"
 
-}
+};
 
 
 
 describe('PostComponent', () => {
 
-    describe('When render', () => {
+    describe('When id exist', () => {
 
-        test("Should render a title Post", async () => {
+        test("Should render only a title Post id and loading", async () => {
+            (useParams as jest.Mock).mockReturnValue({ id: 5 });
 
 
 
             // eslint-disable-next-line testing-library/no-unnecessary-act
+            await act(async () => render(<BrowserRouter><Post /></BrowserRouter>) as any);
 
-            await act(async () => {
+            expect(screen.getByText(`Post 5`)).toBeInTheDocument();
+            expect(screen.getByText("Loading...")).toBeInTheDocument();
 
-                render(
 
-                    <BrowserRouter><Post /></BrowserRouter>); return
+        });
 
-            })
-
-            expect(screen.getByText("Post")).toBeInTheDocument();
-
-        })
-
-        test("it should render post title and post body", async () => {
+        test("should render post title and post body after getPostById", async () => {
 
             (getPostById as jest.Mock).mockImplementation(() => {
 
                 return Promise.resolve(mockPost);
 
-                });
+            });
 
             // eslint-disable-next-line testing-library/no-unnecessary-act
-
             await act(async () => {
 
                 render(
@@ -98,8 +89,42 @@ describe('PostComponent', () => {
 
             expect(screen.getByText("bodychupipandi")).toBeInTheDocument();
 
-        })
+        });
 
-    })
+    });
+
+    describe("When Link exist", () => {
+
+        test("Should be rendered with text Back to home", async () => {
+            await act(async () => render(<BrowserRouter><Post /></BrowserRouter>) as any);
+
+            expect(screen.getByText("Back to Home")).toBeInTheDocument();
+        });
+
+        test("Can be clicked", async () => {
+            await act(async () => render(<BrowserRouter><Post /></BrowserRouter>) as any);
+
+            const backToHomeLink = screen.getByText('Back to Home') as HTMLLinkElement;
+            userEvent.click(backToHomeLink);
+
+            expect(backToHomeLink.href.includes('/post')).toBeTruthy();
+        });
+
+
+    });
+
+    describe("When id doesn't exist", () => {
+        test("Should render only a title Post and loading", async () => {
+            (useParams as jest.Mock).mockReturnValue({ id: "" });
+
+            // eslint-disable-next-line testing-library/no-unnecessary-act
+            await act(async () => render(<BrowserRouter><Post /></BrowserRouter>) as any);
+
+            expect(screen.getByText(`Post`)).toBeInTheDocument();
+            expect(screen.getByText("Loading...")).toBeInTheDocument();
+        });
+
+
+    });
 
 })
