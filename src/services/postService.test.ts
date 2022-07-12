4f6273a2b52187399e26a66b0b63f344847361ae
Ana Valdemoro <ana_v_r97@hotmail.com>
2022-07-12 18:52:57 +0100

    FIX: added test to service

--- src/services/postService.test.ts
+++ src/services/postService.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-conditional-expect */
 import { getPostById } from './postService';
 
 const mockedSuccessResponse = '::mockedSuccessResponse::';
@@ -13,12 +14,36 @@ afterEach(() => {
     jest.clearAllMocks();
 });
 
+afterAll(() => {
+    jest.resetAllMocks();
+});
+
 describe('getPostById', () => {
-    test('should call getPostById and return users', async () => {
+    test('should call getPostById and return post', async () => {
         const postId = '::postId::';
         (global.fetch as jest.Mock).mockImplementation(() => Promise.resolve(mockedFetch));
         const fetchCall = await getPostById(postId);
         expect(global.fetch).toHaveBeenCalledTimes(1);
         expect(fetchCall).toEqual(mockedSuccessResponse);
+    });
+
+    test.only('Should call getPostById and return 404', async () => {
+        const mockedNotFound = {
+            status: 404,
+            statusText: "Not found"
+        };
+        const postId = '::postId::';
+        (global.fetch as jest.Mock).mockImplementation(() => Promise.resolve(mockedNotFound));
+        try {
+            await getPostById(postId)
+            expect(global.fetch).toHaveBeenCalledTimes(1);
+        } catch (error) {
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(error).toBeDefined();
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect((error as any).message).toBe(mockedNotFound.statusText);
+
+        }
+
     });
 });
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -2,5 +2,14 @@ export const getPostById = async (postId: string): Promise<any> => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
+
+  if (response.status === 404) {
+    throw new Error(response.statusText);
+  }
+
+  if (response.status !== 200) {
+    throw new Error("Aqui ha petado");
+  }
+
   return response.json();
 };
